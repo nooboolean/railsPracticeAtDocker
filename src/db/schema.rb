8afca92a703f34665296aaedc26d4e152d35df28
nooboolean <nooboolean@i.softbank.jp>
2021-03-18 17:27:28 +0900

    基本的なCRUDやってみた

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_095816) do
+ActiveRecord::Schema.define(version: 2021_03_17_161229) do
+
+  create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
