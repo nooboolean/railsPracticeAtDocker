fa7d5b45113d4441a924c1493770ddea272e2733
nooboolean <nooboolean@i.softbank.jp>
2021-03-18 22:48:34 +0900

    制約変更などのマイグレーションやってみた

--- src/db/schema.rb
+++ src/db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_17_161229) do
+ActiveRecord::Schema.define(version: 2021_03_18_134249) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 30, null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
