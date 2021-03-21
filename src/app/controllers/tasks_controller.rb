ae41f15e28210c5a0300e662420e7e094350e591
nooboolean <nooboolean@i.softbank.jp>
2021-03-22 00:13:54 +0900

    auto_linkの設置

--- src/app/controllers/tasks_controller.rb
+++ src/app/controllers/tasks_controller.rb
@@ -1,11 +1,10 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-
   def index
     @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
@@ -22,25 +21,24 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = current_user.tasks.find(params[:id])
   end
 
   def update
-    @task.update!(task_params)
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を更新しました"
+    task = current_user.tasks.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を更新しました"
   end
 
   def destroy
-    @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました"
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました"
   end
 
   private
 
   def task_params
     params.require(:task).permit(:name, :description)
-  end
-
-  def set_task
-    @task = current_user.tasks.new(task_params)
   end
 end
